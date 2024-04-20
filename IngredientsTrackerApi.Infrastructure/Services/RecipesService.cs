48ecb88bec76bc4e535b46a163beadad6b657b48
shchoholiev <sh.codework@gmail.com>
2024-04-20 03:35:12 +0000

    Add recipe search by description

--- IngredientsTrackerApi.Infrastructure/Services/RecipesService.cs
+++ IngredientsTrackerApi.Infrastructure/Services/RecipesService.cs
@@ -35,73 +35,6 @@ public class RecipesService(
 
     private readonly IMapper _mapper = mapper;
 
-    public async Task<ProductDto> CreateProductAsync(ProductCreateDto productCreateDto, CancellationToken cancellationToken)
-    {
-        _logger.LogInformation($"Creating a new product with name: {productCreateDto.Name}.");
-
-        var product = _mapper.Map<Product>(productCreateDto);
-        product.CreatedById = GlobalUser.Id.Value;
-        product.CreatedDateUtc = DateTime.UtcNow;
-
-        var createdProduct = await _productsRepository.AddAsync(product, cancellationToken);
-        var productDto = _mapper.Map<ProductDto>(createdProduct);
-
-        _logger.LogInformation($"Product with Id {productDto.Id} is created.");
-
-        return productDto;
-    }
-
-    public async Task<PagedList<ProductDto>> GetProductsPageAsync(
-        int page, int size, string groupId, string? search, CancellationToken cancellationToken)
-    {
-        _logger.LogInformation($"Getting a page of products. Page: {page}, Size: {size}, GroupId: {groupId}.");
-
-        var groupObjectId = ObjectId.Parse(groupId);
-        Expression<Func<Product, bool>> predicate = p => p.GroupId == groupObjectId;
-        if (!string.IsNullOrEmpty(search))
-        {
-            predicate = predicate.And(p => Regex.IsMatch(p.Name, search, RegexOptions.IgnoreCase)
-                || Regex.IsMatch(p.Description, search, RegexOptions.IgnoreCase));
-        }
-
-        var productsTask = _productsRepository.GetPageAsync(page, size, predicate, cancellationToken);
-        var totalCountTask = _productsRepository.GetCountAsync(predicate, cancellationToken);
-
-        await Task.WhenAll(productsTask, totalCountTask);
-
-        var products = await productsTask;
-        var totalCount = await totalCountTask;
-
-        var productDtos = _mapper.Map<List<ProductDto>>(products);
-        var pagedList = new PagedList<ProductDto>(productDtos, page, size, totalCount);
-
-        _logger.LogInformation($"Found {productDtos.Count} products.");
-
-        return pagedList;
-    }
-
-    public async Task<ProductDto> UpdateProductCountAsync(string productId, ProductCountUpdateDto productUpdateDto, CancellationToken cancellationToken)
-    {
-        _logger.LogInformation($"Updating product count for product with Id: {productId}.");
-
-        var productObjectId = ObjectId.Parse(productId);
-        var existingProduct = await _productsRepository.GetOneAsync(productObjectId, cancellationToken);
-
-        if (existingProduct == null)
-        {
-            throw new EntityNotFoundException($"Product with Id {productId} not found.");
-        }
-
-        existingProduct.Count = productUpdateDto.Count;
-
-        var updatedProduct = await _productsRepository.UpdateAsync(existingProduct, cancellationToken);
-        var productDto = _mapper.Map<ProductDto>(updatedProduct);
-
-        _logger.LogInformation($"Product count updated for product with Id: {productId}.");
-
-        return productDto;
-    }
-
     public async Task<RecipeDto> CreateRecipeAsync(RecipeCreateDto dto, CancellationToken cancellationToken)
     {
         _logger.LogInformation($"Creating a new recipe with name: {dto.Name}.");
@@ -213,7 +146,8 @@ public class RecipesService(
         Expression<Func<Recipe, bool>> predicate = r => r.GroupId == groupObjectId;
         if (!string.IsNullOrEmpty(search))
         {
-            predicate = predicate.And(r => Regex.IsMatch(r.Name, search, RegexOptions.IgnoreCase));
+            predicate = predicate.And(r => Regex.IsMatch(r.Name, search, RegexOptions.IgnoreCase)
+                || Regex.IsMatch(r.Text, search, RegexOptions.IgnoreCase));
         }
 
         var recipesTask = _recipesRepository.GetPageAsync(pageNumber, pageSize, predicate, cancellationToken);
